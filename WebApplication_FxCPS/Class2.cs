dd71ad5e408c65fa8c25ee4a71445482a6305a5b
Daniel Monroy <dmon@microsoft.com>
2020-02-13 13:16:56 -0800

    Add project files.

---
+++ WebApplication_FxCPS/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication_FxCPS
+{
+    class Class2
+    {
+        Class1 c = new Class1();
+    }
+}
